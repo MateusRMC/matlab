f46ee1aa56fcc969dccd6f22796f7fc71abf1c2c
Mateus Rocha <mateus.rocha1rj@outlook.com>
2024-01-13 01:55:56 -0500

    feita todas as rotas e componentes de home, login e sobre

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/home";
+import Login from "./components/login";
+import Sobre from "./components/sobre";
 
-function App() {
-  return <div>App</div>;
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sobre" element={<Sobre />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App;
---
+++ src/components/login.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class login extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>login</div>
+    )
+  }
+}
+
+export default login
