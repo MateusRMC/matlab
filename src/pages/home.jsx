db9ccc16bddc3e45e9d05066e850fc740b5178f1
Mateus Rocha <mateus.rocha1rj@outlook.com>
2024-01-14 00:11:20 -0500

    basic homepage layout

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import "./home.css";
 
 export class home extends Component {
   static propTypes = {}
 
   render() {
     return (
-      <div>home</div>
-    )
+      <div id="homepage">
+      <h1>Welcome to MatLab!</h1>
+      <h2>This is a news webpage createad by Mateus Rocha in 2024</h2>
+      <h2>Sign In to see the content</h2>
+      <button onClick="">Sign In</button>
+      </div>
+    );
   }
 }
 
