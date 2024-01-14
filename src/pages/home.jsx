b10de04021a615d19e1534b1af70872facd7557c
Mateus Rocha <mateus.rocha1rj@outlook.com>
2024-01-14 13:43:19 -0500

    HOMEPAGE IS DONE! (AESTHETICALLY)

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,18 +1,27 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import "./home.css";
+import "./home.scss";
+import { Outlet, Link } from 'react-router-dom';
 
 export class home extends Component {
   static propTypes = {}
 
   render() {
     return (
+      <>
       <div id="homepage">
       <h1>Welcome to MatLab!</h1>
-      <h2>This is a news webpage createad by Mateus Rocha in 2024</h2>
-      <h2>Sign In to see the content</h2>
-      <button onClick="">Sign In</button>
+      <h2>The News Web Application</h2>
+        <div id="cta">
+          <Link to="/login">
+            <button>Sign In</button>
+          </Link>
+          <br />
+          <Link className='signup' to="/register">Don't have an account? Sign Up</Link>
+          <footer>Made by <a href='https://github.com/MateusRMC'>Mateus Rocha</a></footer>
+        </div>
       </div>
+      </>
     );
   }
 }
